7cc7595fd84b61d20d5bb8e6763d392220b88ac5
dylangriess <dylangriess@gmail.com>
2022-08-24 08:47:31 -0600

    added weather icon, fixed UV index, fixed city input, working on localStorage

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,9 @@ console.log("hello, world");
 //Variable declarations
 var apiKey = "afd3be8e24e844a682d13ba28363015a";
 var searchBtn = $("#citySearchBtn");
-var cityInput = "Denver";
-var cityHistoryUl = $("#cityHistory");
-var savedCities = [];
+var cityInput = $("#enter-city");
+var searchCity = "Denver";
+var cityHistory = $("#cityHistory");
 
 //Current weather variables
 var cityName = $("#cityName");
@@ -14,6 +14,9 @@ var todayTemp = $("#todayTemp");
 var todayWind = $("#todayWind");
 var todayHumidity = $("#todayHumidity");
 var todayUV = $("#todayUV");
+var uV;
+var todayIcon;
+var todayIconImage = $("#todayIconPng");
 
 //Forecast variables
 var date1 = $("#date1");
@@ -45,7 +48,7 @@ var humidity5 = $("#humidity5");
 function getWeather() {
   var requestUrl =
     "https://api.weatherbit.io/v2.0/current?&city=" +
-    cityInput +
+    searchCity +
     "&units=imperial&key=" +
     apiKey;
   console.log(requestUrl);
@@ -57,27 +60,28 @@ function getWeather() {
       console.log(data);
       cityName.text(data.data[0].city_name);
       todayDate.text(data.data[0].datetime);
+      todayIcon = data.data[0].weather.icon;
+      todayIconImage.attr(
+        "src",
+        `https://www.weatherbit.io/static/img/icons/${todayIcon}.png`
+      );
       todayTemp.text("Temperature: " + data.data[0].temp + " ºF");
       todayWind.text("Wind: " + data.data[0].wind_spd + " MPH");
       todayHumidity.text("Humidity: " + data.data[0].rh + " %");
       todayUV.text("UV Index: " + data.data[0].uv);
-      getUV();
+      uV = data.data[0].uv;
+      if (uV > 6) {
+        todayUV.addClass("severe");
+      } else if (uV > 3 && uV < 5.99) {
+        todayUV.addClass("moderate");
+      } else {
+        todayUV.addClass("favorable");
+      }
     });
 
-  //TODO: add background to UV: favorable, moderate, severe
-  function getUV() {
-    var todayUV = $("#todayUV");
-    if (todayUV.val() > moderate) {
-      todayUV.style = "color: red";
-    } else if (todayUV.val() > favorable) {
-      todayUV.style = "color: yellow";
-    } else {
-      todayUV.style = "color: green";
-    }
-  }
   var forecastUrl =
     "https://api.weatherbit.io/v2.0/forecast/daily&days=[5]?city=" +
-    cityInput +
+    searchCity +
     "&units=imperial&key=" +
     apiKey;
   console.log(forecastUrl);
@@ -87,43 +91,68 @@ function getWeather() {
     })
     .then(function (data) {
       console.log(data);
-      date1.text("Date: " + data.data[0].datetime);
-      temp1.text("Temp: " + data.data[0].temp + " ºF");
-      wind1.text("Wind: " + data.data[0].wind_spd + " MPH");
-      humidity1.text("Humidity: " + data.data[0].rh + " %");
+      date1.text("Date: " + data.data[1].datetime);
+      temp1.text("Temp: " + data.data[1].temp + " ºF");
+      wind1.text("Wind: " + data.data[1].wind_spd + " MPH");
+      humidity1.text("Humidity: " + data.data[1].rh + " %");
 
-      date2.text("Date: " + data.data[1].datetime);
-      temp2.text("Temp: " + data.data[1].temp + " ºF");
-      wind2.text("Wind: " + data.data[1].wind_spd + " MPH");
-      humidity2.text("Humidity: " + data.data[1].rh + " %");
+      date2.text("Date: " + data.data[2].datetime);
+      temp2.text("Temp: " + data.data[2].temp + " ºF");
+      wind2.text("Wind: " + data.data[2].wind_spd + " MPH");
+      humidity2.text("Humidity: " + data.data[2].rh + " %");
 
-      date3.text("Date: " + data.data[2].datetime);
-      temp3.text("Temp: " + data.data[2].temp + " ºF");
-      wind3.text("Wind: " + data.data[2].wind_spd + " MPH");
-      humidity3.text("Humidity: " + data.data[2].rh + " %");
+      date3.text("Date: " + data.data[3].datetime);
+      temp3.text("Temp: " + data.data[3].temp + " ºF");
+      wind3.text("Wind: " + data.data[3].wind_spd + " MPH");
+      humidity3.text("Humidity: " + data.data[3].rh + " %");
 
-      date4.text("Date: " + data.data[3].datetime);
-      temp4.text("Temp: " + data.data[3].temp + " ºF");
-      wind4.text("Wind: " + data.data[3].wind_spd + " MPH");
-      humidity4.text("Humidity: " + data.data[3].rh + " %");
+      date4.text("Date: " + data.data[4].datetime);
+      temp4.text("Temp: " + data.data[4].temp + " ºF");
+      wind4.text("Wind: " + data.data[4].wind_spd + " MPH");
+      humidity4.text("Humidity: " + data.data[4].rh + " %");
 
-      date5.text("Date: " + data.data[4].datetime);
-      temp5.text("Temp: " + data.data[4].temp + " ºF");
-      wind5.text("Wind: " + data.data[4].wind_spd + " MPH");
-      humidity5.text("Humidity: " + data.data[4].rh + " %");
+      date5.text("Date: " + data.data[5].datetime);
+      temp5.text("Temp: " + data.data[5].temp + " ºF");
+      wind5.text("Wind: " + data.data[5].wind_spd + " MPH");
+      humidity5.text("Humidity: " + data.data[5].rh + " %");
     });
 }
 
 function citySearch(event) {
-  var cityInput = "Denver";
-  if (!cityInput) {
+  if (!searchCity) {
+    alert("Please enter valid city.");
     return;
+  } else {
+    getWeather();
   }
-  getWeather();
+}
+
+var savedCities = [];
+// if (localStorage.getItem.forEach("savedCities")) {
+//   savedCities = localStorage.getItem("savedCities");
+//   console.log("Saved cities:" + savedCities);
+// } else {
+//   savedCities = [];
+// }
+
+function handleSavedCities() {
+  // var savedCities = JSON.parse(localStorage.getItem("savedCities"));
+  savedCities.forEach((city) => {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(city));
+    cityHistory.append(li);
+  });
 }
 
 //Add event handler for save input
 searchBtn.on("click", function () {
-  console.log(cityInput);
+  console.log(cityInput.val());
+  searchCity = cityInput.val();
+  // savedCities.push(searchCity);
+  // localStorage.setItem("savedCities", JSON.stringify(savedCities));
+  console.log(searchCity);
   citySearch();
 });
+
+getWeather();
+handleSavedCities();
